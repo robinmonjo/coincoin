9adf63a910e7530bdf6e10e564fe10a615bb0555
Robin Monjo <robinmonjo@gmail.com>
2017-06-28 21:46:29 +0200

    basic proof of work

--- apps/blockchain/config/config.exs
+++ apps/blockchain/config/config.exs
@@ -21,13 +21,6 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
-
 config :blockchain,
-  port: String.to_integer(System.get_env("P2P_PORT") || "5000")
+  port: String.to_integer(System.get_env("P2P_PORT") || "5000"),
+  pow_difficulty: if(Mix.env == :test, do: 1, else: 4)
