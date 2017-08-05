4b60200067895fe69cf3536c43e00090931ff423
Robin Monjo <robinmonjo@gmail.com>
2017-08-05 14:13:28 +0200

    upgrade elixir in travis + upgrade to phx 1.3 pfinal

--- apps/blockchain_web/mix.exs
+++ apps/blockchain_web/mix.exs
@@ -32,7 +32,7 @@ defmodule Blockchain.Web.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
+    [{:phoenix, "~> 1.3.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
