788bef7c754ef07f271493f146b79d5a686b1756
Robin Monjo <robinmonjo@gmail.com>
2017-09-14 14:06:22 +0200

    fixture that uses the ledger

--- apps/token/test/support/fixtures.ex
+++ apps/token/test/support/fixtures.ex
@@ -1,8 +1,8 @@
 defmodule Token.Fixtures do
   @moduledoc "Test helpers"
 
-  alias Blockchain.{Block}
-  alias Token.{Wallet, Transaction, Transaction.Verify}
+  alias Token.{Wallet, Transaction, Ledger}
+  alias Blockchain.Chain
 
   # mock a ledger with 3 participants alice bob joe
   # scénario:
@@ -24,42 +24,36 @@ defmodule Token.Fixtures do
   #   - alice/tx1(20)+tx2(7)+tx3(6)+tx4(3) bob/utx(20)+tx4(4) joe/tx2(5)+tx3(4)+tx4(1)
 
   def mock_ledger do
+    :ok = Chain.clear_chain()
+
     # create wallets for each participants
     alice = Wallet.generate_wallet()
     bob = Wallet.generate_wallet()
     joe = Wallet.generate_wallet()
 
     # initial founds
-    alice_utx = Transaction.new_coinbase_transaction([{alice.address, 50}])
-    bob_utx = Transaction.new_coinbase_transaction([{bob.address, 20}])
-    ledger = [alice_utx, bob_utx]
+    alice_utx = Transaction.new_coinbase_transaction([[alice.address, 50]])
+    :ok = Ledger.write(alice_utx)
+    bob_utx = Transaction.new_coinbase_transaction([[bob.address, 20]])
+    :ok = Ledger.write(bob_utx)
 
     # first transaction
-    tx1 = Transaction.new_transaction(alice, [{alice_utx.hash, 0}], [{bob.address, 20}, {joe.address, 10}, {alice.address, 20}])
-    :ok = Verify.verify_transaction(tx1, find_in_ledger_fn(ledger))
-    ledger = [tx1 | ledger]
+    tx1 = Transaction.new_transaction(alice, [[alice_utx.hash, 0]], [[bob.address, 20], [joe.address, 10], [alice.address, 20]])
+    :ok = Ledger.write(tx1)
 
     # second transaction
-    tx2 = Transaction.new_transaction(bob, [{tx1.hash, 0}], [{joe.address, 5}, {alice.address, 7}, {bob.address, 8}])
-    :ok = Verify.verify_transaction(tx2, find_in_ledger_fn(ledger))
-    ledger = [tx2 | ledger]
+    tx2 = Transaction.new_transaction(bob, [[tx1.hash, 0]], [[joe.address, 5], [alice.address, 7], [bob.address, 8]])
+    :ok = Ledger.write(tx2)
 
     # third transaction
-    tx3 = Transaction.new_transaction(joe, [{tx1.hash, 1}], [{alice.address, 6}, {joe.address, 4}])
-    :ok = Verify.verify_transaction(tx3, find_in_ledger_fn(ledger))
-    ledger = [tx3 | ledger]
+    tx3 = Transaction.new_transaction(joe, [[tx1.hash, 1]], [[alice.address, 6], [joe.address, 4]])
+    :ok = Ledger.write(tx3)
+
 
     # fourth transaction
-    tx4 = Transaction.new_transaction(bob, [{tx2.hash, 2}], [{alice.address, 3}, {joe.address, 1}, {bob.address, 4}])
-    :ok = Verify.verify_transaction(tx4, find_in_ledger_fn(ledger))
-    ledger = [tx4 | ledger]
+    tx4 = Transaction.new_transaction(bob, [[tx2.hash, 2]], [[alice.address, 3], [joe.address, 1], [bob.address, 4]])
+    :ok = Ledger.write(tx4)
 
-    %{alice: alice, bob: bob, joe: joe, ledger: ledger}
-  end
-
-  defp find_in_ledger_fn(ledger) do
-    fn(f) ->
-      Enum.find(ledger, &(f.(&1)))
-    end
+    %{alice: alice, bob: bob, joe: joe}
   end
 end
