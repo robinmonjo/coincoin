dbbbf3d565667e2de122e1e7a5d41a6306ceede0
Robin Monjo <robinmonjo@gmail.com>
2017-09-14 14:09:24 +0200

    ledger module for blockchain abstraction

--- apps/token/lib/token/ledger.ex
+++ apps/token/lib/token/ledger.ex
@@ -4,42 +4,62 @@ defmodule Token.Ledger do
   """
 
   alias Token.{Wallet, Transaction}
+  alias Blockchain.Block
+
+  def write(%Transaction{} = tx), do: write(tx, Mix.env)
+  def write(%Transaction{} = tx, :test), do: Blockchain.mine(tx)
+  def write(%Transaction{} = tx, _), do: Blockchain.add(tx)
+
+  def all_transactions() do
+    reduce_while([], &({:cont, [&1 | &2]}))
+  end
+
+  def find_func(), do: find_func(Blockchain.blocks())
+  def find_func(chain) do
+    fn(func) ->
+      reduce_while(chain, nil, fn(%Transaction{} = tx, acc) ->
+        if func.(tx), do: {:halt, tx}, else: {:cont, acc}
+      end)
+    end
+  end
 
   def unspent_outputs(%Wallet{} = wallet) do
-    unspent_outputs(wallet, blockchain_transactions_list())
-  end
-  def unspent_outputs(%Wallet{} = wallet, ledger) do
-    inputs_set = inputs_set(ledger)
-    wallet_outputs = wallet_outputs(wallet, ledger)
-
-    Enum.reject(wallet_outputs, fn({tx_hash, output_index, _}) ->
-      MapSet.member?(inputs_set, {tx_hash, output_index})
+    inputs_set = inputs_set()
+    Enum.reject(wallet_outputs(wallet), fn({tx_hash, output_index, _}) ->
+      MapSet.member?(inputs_set, [tx_hash, output_index])
     end)
   end
 
   # returns all ledger inputs in a MapSet
-  defp inputs_set(ledger) do
-    Enum.reduce(ledger, MapSet.new, fn(%Transaction{inputs: inputs}, set) ->
-      Enum.reduce(inputs, set, &(MapSet.put(&2, &1)))
+  defp inputs_set do
+    reduce_while(MapSet.new, fn(%Transaction{inputs: inputs}, set) ->
+      {:cont, Enum.reduce(inputs, set, &(MapSet.put(&2, &1)))}
     end)
   end
 
   # returns all transaction sent to the given wallet format: [{tx_hash, output_idx, value}]
-  defp wallet_outputs(%Wallet{address: address}, ledger) do
-    Enum.reduce(ledger, [], fn(%Transaction{hash: hash, outputs: outputs}, acc) ->
+  defp wallet_outputs(%Wallet{address: address}) do
+    reduce_while([], fn(%Transaction{hash: hash, outputs: outputs}, acc) ->
       indexed_outputs = Enum.with_index(outputs)
-      acc ++ Enum.reduce(indexed_outputs, [], fn({{recipient, value}, index}, acc) ->
+      {:cont, acc ++ Enum.reduce(indexed_outputs, [], fn({[recipient, value], index}, acc) ->
         if recipient == address do
           [{hash, index, value} | acc]
         else
           acc
         end
-      end)
+      end)}
     end)
   end
 
-  defp blockchain_transactions_list do
-    blocks = Blockchain.blocks()
-    Enum.map(blocks, fn(%Block{data: data}) -> data end)
+  defp reduce_while(acc, func), do: reduce_while(Blockchain.blocks(), acc, func)
+  defp reduce_while(chain, acc, func) do
+    Enum.reduce_while(chain, acc, fn(%Block{data: data}, acc) ->
+      case data do
+        %Transaction{} = data ->
+          func.(data, acc)
+        _ ->
+          {:cont, acc}
+      end
+    end)
   end
 end
