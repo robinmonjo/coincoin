956b8c9d14409bdef429b60e922456679a51f078
Robin Monjo <robinmonjo@gmail.com>
2017-09-14 14:08:06 +0200

    wallet compute balance + write transaction to ledger

--- apps/token/lib/token/wallet.ex
+++ apps/token/lib/token/wallet.ex
@@ -1,7 +1,7 @@
 defmodule Token.Wallet do
   @moduledoc "Wallet module"
 
-  alias Token.{Wallet, Crypto}
+  alias Token.{Wallet, Crypto, Ledger, Transaction}
 
   defstruct [
     :address,
@@ -10,7 +10,7 @@ defmodule Token.Wallet do
   ]
 
   def generate_wallet do
-    {pub, priv} = generate_key_pair()
+    {pub, priv} = Crypto.generate_key_pair()
     %Wallet{
       address: Crypto.public_key_hash(pub),
       public_key: pub,
@@ -18,17 +18,59 @@ defmodule Token.Wallet do
     }
   end
 
-  defp generate_key_pair do
-    {pub, priv} = :crypto.generate_key(:ecdh, :secp256k1)
-    {Base.encode16(pub), Base.encode16(priv)}
-  end
-
-  # return hex signature
   def sign(msg, %Wallet{} = wallet) do
     Crypto.sign(wallet.private_key, msg)
   end
 
   def verify(msg, signature, %Wallet{} = wallet) do
     Crypto.verify_signature(wallet.public_key, msg, signature)
+  end
+
+  def balance(%Wallet{} = wallet) do
+    wallet
+    |> Ledger.unspent_outputs()
+    |> unspent_outputs_sum()
+  end
+
+  def send(value, recipient, %Wallet{} = wallet) do
+    with {:ok, unspent_outputs} <- select_outputs(value, wallet),
+         inputs <- prepare_inputs(unspent_outputs),
+         outputs <- [[recipient, value]],
+         final_outputs <- add_change_output(value, outputs, unspent_outputs, wallet)
+    do
+      tx = Transaction.new_transaction(wallet, inputs, final_outputs)
+      Ledger.write(tx)
+    end
+  end
+
+  defp add_change_output(value, outputs, unspent_outputs, %Wallet{} = wallet) do
+    outputs_sum = unspent_outputs_sum(unspent_outputs)
+    if outputs_sum > value do
+      [[wallet.address, outputs_sum - value] | outputs]
+    else
+      outputs
+    end
+  end
+
+  defp prepare_inputs(outputs), do: prepare_inputs(outputs, [])
+  defp prepare_inputs([], inputs), do: inputs
+  defp prepare_inputs([{tx_hash, index, _value} | remaining], inputs) do
+    prepare_inputs(remaining, [[tx_hash, index] | inputs])
+  end
+
+  defp select_outputs(target_value, %Wallet{} = wallet) do
+    wallet
+    |> Ledger.unspent_outputs()
+    |> Enum.sort(fn({_, _, v1}, {_, _, v2}) -> v1 <= v2 end)
+    |> select_outputs(target_value, [])
+  end
+  defp select_outputs(_, value, outputs) when value <= 0, do: {:ok, outputs}
+  defp select_outputs([], _value, _outputs), do: {:error, "not enough coins"}
+  defp select_outputs([{_, _, v} = output | remaining], value, outputs) do
+    select_outputs(remaining, value - v, [output | outputs])
+  end
+
+  defp unspent_outputs_sum(unspent_outputs) do
+    Enum.reduce(unspent_outputs, 0, fn({_, _, value}, acc) -> acc + value end)
   end
 end
--- apps/token/test/token/wallet_test.exs
+++ apps/token/test/token/wallet_test.exs
@@ -1,14 +1,18 @@
 defmodule Token.WalletTest do
   use ExUnit.Case
+  import Token.Fixtures
 
-  alias Token.Wallet
+  alias Token.{Wallet, Ledger}
 
-  setup do
-    w = Wallet.generate_wallet()
-    {:ok, wallet: w}
+  setup tags do
+    if tags[:mock_ledger] do
+      {:ok, mock_ledger()}
+    else
+      {:ok, wallet: Wallet.generate_wallet()}
+    end
   end
 
-  test "generate a new wallet", %{wallet: w} do
+  test "generate wallet", %{wallet: w} do
     assert w.public_key != nil
     assert w.private_key != nil
     assert byte_size(w.address) == 40
@@ -20,4 +24,79 @@ defmodule Token.WalletTest do
     assert Wallet.verify(data, signature, w)
   end
 
+  @tag mock_ledger: true
+  test "balance", %{alice: alice, bob: bob, joe: joe} do
+    assert Wallet.balance(alice) == 36
+    assert Wallet.balance(bob) == 24
+    assert Wallet.balance(joe) == 10
+  end
+
+  @tag mock_ledger: true
+  test "send", %{alice: alice, bob: bob, joe: joe} do
+    compute_balances = fn() ->
+      for p <- [alice, bob, joe], do: Wallet.balance(p)
+    end
+
+    [a0, b0, j0] = compute_balances.()
+
+    # first transaction
+    amount = 10
+    assert Wallet.send(amount, bob.address, alice) == :ok
+
+    [a1, b1, j1] = compute_balances.()
+
+    assert a1 == a0 - amount # 26
+    assert b1 == b0 + amount # 34
+    assert j1 == j0 # 10
+
+    # second transaction
+    amount = 13
+    assert Wallet.send(amount, joe.address, alice) == :ok
+
+    [a2, b2, j2] = compute_balances.()
+
+    assert a2 == a1 - amount # 13
+    assert b2 == b1 # 34
+    assert j2 == j1 + amount # 23
+
+    # third transaction
+    amount = 1
+    assert Wallet.send(amount, bob.address, joe) == :ok
+
+    [a3, b3, j3] = compute_balances.()
+
+    assert a3 == a2 # 13
+    assert b3 == b2 + amount # 35
+    assert j3 == j2 - amount # 23
+
+    # fourth transaction
+    amount = 35
+    assert Wallet.send(amount, joe.address, bob) == :ok
+
+    [a4, b4, j4] = compute_balances.()
+
+    assert a4 == a3 # 13
+    assert b4 == b3 - amount # 0
+    assert j4 == j3 + amount # 58
+
+    # fifth transaction
+    amount = 19
+    assert {:error, _reason} = Wallet.send(amount, bob.address, alice)
+
+    [a5, b5, j5] = compute_balances.()
+
+    assert a5 == a4 # 13
+    assert b5 == b4 # 0
+    assert j5 == j4 # 58
+
+    # sixth transaction
+    amount = 29
+    assert Wallet.send(amount, bob.address, joe) == :ok
+
+    [a6, b6, j6] = compute_balances.()
+
+    assert a6 == a5 # 13
+    assert b6 == b5 + amount # 29
+    assert j6 == j5 - amount # 29
+  end
 end
