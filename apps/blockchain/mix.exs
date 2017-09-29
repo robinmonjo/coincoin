746e3db733e42d767c5ba3c9b6c5918315f803f3
Robin Monjo <robinmonjo@gmail.com>
2017-09-29 16:33:25 +0200

    bump elixir version in mix files

--- apps/blockchain/mix.exs
+++ apps/blockchain/mix.exs
@@ -8,7 +8,7 @@ defmodule Blockchain.Mixfile do
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
