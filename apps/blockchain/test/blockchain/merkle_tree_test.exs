0fa4b22a4ec9b6735ac7b3ddacd2adf8b1d97ea6
Robin Monjo <robinmonjo@gmail.com>
2017-10-23 20:20:21 +0200

    module to work with merkle tree

---
+++ apps/blockchain/test/blockchain/merkle_tree_test.exs
@@ -0,0 +1,28 @@
+defmodule Blockchain.MerkleTreeTest do
+  use ExUnit.Case, async: true
+
+  alias Blockchain.MerkleTree
+
+  setup do
+    #        abdcefghi
+    #
+    #      abcdefgh     i
+    #
+    #    abcd     efgh      i
+    #
+    #  ab     cd    ef   gh    i
+    #
+    # a   b   c   d   e   f   g   h   i
+
+    {:ok, leaves: ["a", "b", "c", "d", "e", "f", "g", "h", "i"]}
+  end
+
+  test "build a tree and access its route", %{leaves: leaves} do
+    # this works because BitString implements Blockchain.BlockData
+    tree = MerkleTree.build(leaves)
+    assert length(tree) == 20
+    assert MerkleTree.root(leaves) == "29D9F10AEFD064E2232A9B585D175366D72163E998D8E97F1AA7E6885664687C"
+  end
+end
+
+
