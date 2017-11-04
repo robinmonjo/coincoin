d480b1e473702ce9943efc87587ea623ae679081
Robin Monjo <robinmonjo@gmail.com>
2017-11-04 22:10:11 +0100

    setting dev version

--- apps/token/mix.exs
+++ apps/token/mix.exs
@@ -8,7 +8,7 @@ defmodule Token.Mixfile do
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
-     elixir: "~> 1.6",
+     elixir: "~> 1.6-dev",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
